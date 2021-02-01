f2eca694cae7093f4831c4eba7e124d0b416ee39
Chris Husslack <chris.husslack@siemens-healthineers.com>
2021-02-01 10:20:24 +0100

    Added evaluation of binding security for net tcp and named pipe binding.

---
+++ WcfDumper/DataModel/NetNamedPipeBindingSecurity.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace WcfDumper.DataModel
+{
+    /// <summary>
+    /// Binding security for named pipe bindings.
+    /// </summary>
+    public class NetNamedPipeBindingSecurity : IBindingSecurity
+    {
+        public NetNamedPipeSecurityMode SecurityMode;
+
+        public override string ToString()
+        {
+            return $"Security Mode: {SecurityMode}";
+        }
+    }
+}
---
+++ WcfDumper/DataModel/NetTcpBindingSecurity.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+
+namespace WcfDumper.DataModel
+{
+    /// <summary>
+    /// Binding security for net tcp bindings.
+    /// </summary>
+    public class NetTcpBindingSecurity : IBindingSecurity
+    {
+        public SecurityMode SecurityMode;
+        public TcpClientCredentialType ClientCredentialType;
+
+        public override string ToString()
+        {
+            return $"Security Mode: '{SecurityMode}' Client Credential Type: '{ClientCredentialType}'";
+        }
+    }
+}
